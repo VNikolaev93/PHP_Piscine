624b7ca07585d1fa3744c13490f5616a28347ab6
Viacheslav Nikolaev <45995392+bsharna@users.noreply.github.com>
2019-05-07 17:55:11 +0300

    Add files via upload

--- day04/ex01/create.php
+++ day04/ex01/create.php
@@ -1,9 +1,29 @@
 <?php
-if ($_POST["login"] && $_POST["passwd"] && $_POST["submit"] == "OK")
-{
-    if (!(file_exists("../private"))) //если папки Private нет
-        mkdir("../private");         //то создаем ее
-    if (!(file_exists("../private/passwd")))        //если файла passwd нет
-        file_put_contents("../private/passwd");     //то создаем его
-
-}
+    if ($_POST['login'] && $_POST['passwd'] && $_POST['submit'] == "OK") {
+        if (!file_exists('../private')) {
+            mkdir("../private");
+        }
+        if (!file_exists('../private/passwd')) {
+            file_put_contents('../private/passwd', null);
+        }
+        $user = unserialize(file_get_contents('../private/passwd'));
+        if ($user) {
+            $ex_user = 0;
+            foreach ($user as $key => $value) {
+                if ($value['login'] === $_POST['login'])
+                    $ex_user = 1;
+            }
+        }
+        if ($ex_user) {
+            echo "ERROR\n";
+        } else {
+            $temp['login'] = $_POST['login'];
+            $temp['passwd'] = hash('whirlpool', $_POST['passwd']);
+            $user[] = $temp;
+            file_put_contents('../private/passwd', serialize($user));
+            echo "OK\n";
+        }
+    } else {
+        echo "ERROR\n";
+    }
+?>
